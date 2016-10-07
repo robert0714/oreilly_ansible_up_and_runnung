0b87d85baf4d0f4b3bc6b2fc14a49112803ed944
robert0714 <robert0714@gmaill.com>
2016-10-08 00:16:42 +0800

    adjust

---
+++ ch04/playbooks/scripts/ubuntu.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Installing Ansible..."
+
+apt-get install -y software-properties-common
+apt-add-repository ppa:ansible/ansible
+apt-get update
+apt-get install -y --force-yes ansible
+apt-get install -y jq
+apt-get install -y python-pip 
+
+cp /vagrant/ansible.cfg /etc/ansible/ansible.cfg
+
+pip install --upgrade pip
+pip install paramiko
+pip install docker-py boto
