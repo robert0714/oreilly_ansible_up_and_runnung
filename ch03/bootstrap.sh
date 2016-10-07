2cc0c61871477a828acd36a95fcc9561a257aab0
robert0714 <robert0714@gmaill.com>
2016-10-08 00:02:34 +0800

    adjust

--- ch03/bootstrap.sh
+++ ch03/bootstrap.sh
@@ -5,11 +5,10 @@ echo "Installing Ansible..."
 su -c 'rpm -Uvh http://download.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-8.noarch.rpm'
 yum -y install ansible
 yum -y install jq
-
-yum -y install epel-release
 yum -y install python-pip 
 
 cp /vagrant/ansible.cfg /etc/ansible/ansible.cfg
 
 pip install --upgrade pip
 pip install paramiko
+pip install docker-py boto
