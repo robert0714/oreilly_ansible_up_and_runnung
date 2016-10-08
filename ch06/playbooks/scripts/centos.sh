ca7c6ec5c1b6af2b4abc9c8632357517ce2ab769
robert0714 <robert0714@gmaill.com>
2016-10-08 22:10:05 +0800

    adjust

---
+++ ch06/playbooks/scripts/centos.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Installing Ansible..."
+
+su -c 'rpm -Uvh http://download.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-8.noarch.rpm'
+yum -y install ansible
+yum -y install jq
+yum -y install python-pip 
+
+cp /vagrant/ansible.cfg /etc/ansible/ansible.cfg
+
+pip install --upgrade pip
+pip install paramiko
+pip install docker-py boto
